67dc1c81b82079b9f6cd8bd66cbca507a891f7d0
zeyad03 <122759970+zeyad03@users.noreply.github.com>
2025-02-02 19:41:15 +0000

    updated setup script with specific versions for consistant build across any device

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
 requires = [
-    'flask',
-    'spotipy',
-    'html5lib',
-    'requests',
-    'requests_html',
-    'beautifulsoup4',
-    'yt_dlp',
-    'pathlib',
-    'pandas'
+    'flask>=2.0.0',
+    'spotipy>=2.19.0',
+    'html5lib>=1.1',
+    'requests>=2.26.0',
+    'requests_html>=0.10.0',
+    'beautifulsoup4>=4.10.0',
+    'yt-dlp>=2023.7.6',
+    'pytube>=12.0.0',
+    'ffmpeg-python>=0.2.0',
+    'pathlib>=1.0.1',
+    'pandas>=1.3.0',
+    'urllib3<2.0.0,>=1.25.8'
 ]
 
 setup(
